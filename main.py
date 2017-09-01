dec7e29cdbd1fd76bde2862e31c2bfdb76b8f534
Jonathan <jonathan.plotkin@nokia.com>
2017-09-01 22:09:19 +0300

    ongoing

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 at = 'o.se1YZX6plVhVE84GqAOno53T5mw3MjDL'
 from websocket import create_connection
 import time, json
-import mysql.connector
+import mysql.connector as mariadb
+mariadb_connection = mariadb.connect(user='spyai', password='bazinga', database='spyai')
+cursor = mariadb_connection.cursor()
+
 
 
 class Pushparser:
+
     def parse(self, obj):
         parsedJson = json.loads(obj)
         type = parsedJson['type']
+        appName = ''
+        sender = ''
+        body = ''
 
         if type == 'push':
             pushData = parsedJson['push']
@@ -17,18 +24,22 @@ class Pushparser:
 
             if pushType == 'mirror':
                 appName = pushData.get('application_name', None)
-                title = pushData.get('title', None)
+                sender = pushData.get('title', None)
                 body = pushData.get('body', None)
-                print('%s Message received from user: %s with text: %s' % (appName, title, body))
+                print('%s Message received from user: %s with text: %s' % (appName, sender, body))
 
             elif pushType == 'sms_changed':
-                title = pushData['notifications'][0].get('title', None)
+                appName = 'sms'
+                sender = pushData['notifications'][0].get('title', None)
                 body = pushData['notifications'][0].get('body', None)
-                print('%s Message received from user: %s with text: %s' % ('sms', title, body))
+                print('%s Message received from user: %s with text: %s' % ('sms', sender, body))
 
             else:
                 print('new event : %s' % pushType)
                 print(parsedJson)
+
+            queryStr = 'insert into data_bin (CONTACT_NAME, MSG_TEXT, APP) VALUES ({}, {}, {})'.format(sender,body,appName)
+            cursor.execute(queryStr)
 
         elif type == 'nop':
             print('new event : keepalive')
